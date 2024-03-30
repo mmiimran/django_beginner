9508e64604e8fd23de8a7466e26eb488d3c89967
mmiimran <go2imran6@gmail.com>
2024-03-30 22:40:05 +0600

    first commit

---
+++ 2.hello_world_app/pages/views.py
@@ -0,0 +1,7 @@
+# pages/views
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def homePageView(request):
+	return HttpResponse('Hello, World')
