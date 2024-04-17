134c4b564fd304cc75b3408dbea10b656c43c248
mmiimran <go2imran6@gmail.com>
2024-04-18 00:22:45 +0600

    forms create edit update delete

---
+++ 06.Forms/django_project/urls.py
@@ -0,0 +1,9 @@
+#django_project/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("blog.urls")),
+]
